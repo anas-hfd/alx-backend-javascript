227fc60a2982b23c141cacab86706e63971038c1
anas-hfd <anas1.hfd@gmail.com>
2024-01-08 12:08:27 +0000

    array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+function getListStudents() {
+	const array = [{id: 1, firstName: 'Guillaume', location: 'San Fransisco'},
+		id: 2, firstName: 'James', location: 'Columbia'}
+		id: 5, firName: 'Serena', location: 'San Fransisco'}
+	]
+	return array;
+}
