af4926066f393f28ef115bff2e1d0faad7cacb96
anas-hfd <anas1.hfd@gmail.com>
2024-01-09 15:46:12 +0100

    ids sum

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray) {
+    return [];
+  }
+  return students.reduce((accumilator, currentStudent) => accumilator + currentStudent.id)
+}
